536ca6aa116e3aa19f354e5f65cd2c4fa6712fea
kkolada <krzysztof.kolada@gmail.com>
2018-02-18 22:26:43 +0100

    Initial app commit ('create-react-app' configuration for SCSS + Bootstrap Sass).

---
+++ src/components/test-button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TestButton = () => (
+  <p className="App-intro">
+    Bootstrap@4 test button - <button class="btn btn-success">BS success</button>
+  </p>
+);
+
+export default TestButton;
